948ae6c820d295dd5f9f9a4154653a9518c59a3c
Antti Rastas <antti.rastas@gmail.com>
2018-03-25 11:28:59 +0300

    Create tests for Maksukortti class

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,40 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals(10, this.kortti.saldo());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        this.kortti.lataaRahaa(4);
+        assertEquals(14, this.kortti.saldo());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaaTarpeeksi() {
+        this.kortti.otaRahaa(3);
+        assertEquals(7, this.kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        this.kortti.otaRahaa(15);
+        assertEquals(10, this.kortti.saldo());
+    }
+    
+    @Test
+    public void metodiPalauttaaTrueJosRahatRiittivatJaMuutenFalse() {
+        assertTrue(this.kortti.otaRahaa(7));
+        assertFalse(this.kortti.otaRahaa(17));
+    }
+    
+    @Test
+    public void toStringMetodiToimii() {
+        assertEquals("saldo: 0.10", this.kortti.toString());
+        this.kortti.lataaRahaa(150);
+        assertEquals("saldo: 1.60", this.kortti.toString());
+    }
 }
